5082a3364ea03c0291c121f68ea08f9fdcc75fbb
Suvarna Rokade <suvarna@stackgen.com>
2025-12-23 15:56:42 +0530

    use dynamodb

--- main.tf
+++ main.tf
@@ -1,27 +1,75 @@
-module "s3_bucket" {
-  source  = "git::https://github.com/Rchanger/s3module//?ref=main"
-  version = "~> 4.0"
+# Create DynamoDB table
+resource "aws_dynamodb_table" "this" {
+  name             = var.name
+  billing_mode     = var.billing_mode
+  hash_key         = var.hash_key
+  range_key        = var.range_key
+  read_capacity    = var.read_capacity
+  write_capacity   = var.write_capacity
+  stream_enabled   = var.stream_enabled
+  stream_view_type = var.stream_view_type
+  tags             = var.tags
 
-  bucket = var.bucket_name
-  acl    = var.acl
+  dynamic "attribute" {
+    for_each = var.attribute
+    content {
+      name = attribute.value.name
+      type = attribute.value.type
+    }
+  }
 
-  control_object_ownership = var.control_object_ownership
-  object_ownership         = var.object_ownership
+  dynamic "global_secondary_index" {
+    for_each = var.global_secondary_index
+    content {
+      name            = global_secondary_index.value.name
+      hash_key        = global_secondary_index.value.hash_key
+      range_key       = global_secondary_index.value.range_key
+      projection_type = global_secondary_index.value.projection_type
+      read_capacity   = global_secondary_index.value.read_capacity
+      write_capacity  = global_secondary_index.value.write_capacity
+    }
+  }
 
-  versioning = var.versioning
+  dynamic "local_secondary_index" {
+    for_each = var.local_secondary_index
+    content {
+      name            = local_secondary_index.value.name
+      range_key       = local_secondary_index.value.range_key
+      projection_type = local_secondary_index.value.projection_type
+    }
+  }
 
-  server_side_encryption_configuration = var.server_side_encryption_configuration
+  dynamic "point_in_time_recovery" {
+    for_each = var.point_in_time_recovery
+    content {
+      enabled = point_in_time_recovery.value.enabled
+    }
+  }
 
-  lifecycle_rule = var.lifecycle_rule
+  dynamic "server_side_encryption" {
+    for_each = var.server_side_encryption != null ? var.server_side_encryption : []
+    content {
+      enabled     = server_side_encryption.value.enabled
+      kms_key_arn = server_side_encryption.value.kms_key_arn
+    }
+  }
 
-  # Block public access
-  block_public_acls       = var.block_public_acls
-  block_public_policy     = var.block_public_policy
-  ignore_public_acls      = var.ignore_public_acls
-  restrict_public_buckets = var.restrict_public_buckets
+  dynamic "ttl" {
+    for_each = var.ttl
+    content {
+      attribute_name = ttl.value.attribute_name
+      enabled        = ttl.value.enabled
+    }
+  }
 
-  tags = {
-    Environment = var.environment
-    Terraform   = "true"
+  dynamic "timeouts" {
+    for_each = var.timeouts != null ? [var.timeouts] : []
+    content {
+      create = lookup(timeouts.value, "create", null)
+      delete = lookup(timeouts.value, "delete", null)
+      update = lookup(timeouts.value, "update", null)
+    }
   }
 }
+
+
--- variables.tf
+++ variables.tf
@@ -1,77 +1,110 @@
-variable "environment" {
-  description = "Environment name"
+
+variable "attribute" {
+  description = "The attribute of the DynamoDB table"
+  type        = list(object({ name: string, type: string }))
+  default     = [{ name = "id", type = "S" }]
+  nullable    = false
+}
+
+variable "billing_mode" {
+  description = "The billing mode of the DynamoDB table"
   type        = string
+  default     = "PROVISIONED"
+  nullable    = true
 }
 
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
-
-variable "acl" {
-  description = "ACL for the bucket"
-  type        = string
-  default     = "private"
-}
-
-variable "control_object_ownership" {
-  description = "Whether to manage S3 Bucket Ownership Controls"
-  type        = bool
-  default     = true
-}
-
-variable "object_ownership" {
-  description = "Object ownership"
-  type        = string
-  default     = "BucketOwnerPreferred"
-}
-
-variable "versioning" {
-  description = "Versioning configuration"
-  type        = any
-  default     = {
-    enabled = true
-  }
-}
-
-variable "server_side_encryption_configuration" {
-  description = "Server-side encryption configuration"
-  type        = any
-  default     = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-variable "lifecycle_rule" {
-  description = "Lifecycle rules"
-  type        = any
+variable "global_secondary_index" {
+  description = "A list of maps representing the global secondary indexes"
+  type        = list(object({ name: string, hash_key: string, range_key: string, projection_type: string, read_capacity: number, write_capacity: number }))
   default     = []
+  nullable    = true
 }
 
-variable "block_public_acls" {
-  description = "Block public ACLs"
-  type        = bool
-  default     = true
+variable "hash_key" {
+  description = "The hash key of the DynamoDB table"
+  type        = string
+  default     = "id"
+  nullable    = false
 }
 
-variable "block_public_policy" {
-  description = "Block public policy"
-  type        = bool
-  default     = true
+variable "local_secondary_index" {
+  description = "A list of maps representing the local secondary indexes"
+  type        = list(object({ name: string, range_key : string, projection_type: string}))
+  default     = []
+  nullable    = true
 }
 
-variable "ignore_public_acls" {
-  description = "Ignore public ACLs"
-  type        = bool
-  default     = true
+variable "point_in_time_recovery" {
+  description = "Point-in-time recovery options"
+  type        = list(object({ enabled: bool }))
+  default     = []
+  nullable    = true
 }
 
-variable "restrict_public_buckets" {
-  description = "Restrict public buckets"
+variable "range_key" {
+  description = "The range key of the DynamoDB table"
+  type        = string
+  default     = ""
+  nullable    = true
+}
+
+variable "server_side_encryption" {
+  description = "Encryption at rest options. AWS DynamoDB tables are automatically encrypted at rest with an AWS-owned Customer Master Key if this argument isn't specified."
+  type        = list(object({ enabled: bool, kms_key_arn: string }))
+  default     = []
+  nullable    = true
+}
+
+variable "stream_enabled" {
+  description = "Indicates whether Streams is to be enabled (true) or disabled (false)"
   type        = bool
-  default     = true
+  nullable    = true
+}
+
+variable "stream_view_type" {
+  description = "Indicates the DynamoDB Streams settings for the table"
+  type        = string
+  default     = "NEW_IMAGE"
+  nullable    = true
+}
+
+variable "name" {
+  description = "The name of the DynamoDB table"
+  type        = string
+  default     = "my-table"
+  nullable    = false
+}
+
+variable "tags" {
+  description = "A map of tags to apply to the resources"
+  type        = map(string)
+  default     = {}
+  nullable    = true
+}
+
+variable "ttl" {
+  description = "A map representing the TTL settings"
+  type        = list(object({ attribute_name: string, enabled: bool }))
+  default     = []
+  nullable    = true
+}
+
+variable "write_capacity" {
+  description = "Number of write units for this table. If the billing_mode is PROVISIONED, this field is required."
+  type        = number
+  default     = 1
+  nullable    = true
+}
+
+variable "read_capacity" {
+  description = "Number of read units for this table. If the billing_mode is PROVISIONED, this field is required."
+  type        = number
+  default     = 1
+  nullable    = true
+}
+
+variable "timeouts" {
+  description = "The timeouts for create, update and delete operations"
+  type        = object({create: optional(string), update: optional(string), delete: optional(string)})
+  nullable    = true
 }
